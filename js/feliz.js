10a83706c2388829c8cc680e5cfb2ea4c354be3b
Sofia Furno <furnosofia@gmail.com>
2022-10-05 18:13:05 -0300

    librerias

--- js/feliz.js
+++ js/feliz.js
@@ -1,9 +1,9 @@
 let musicaRock = document.getElementById("boton1");
 let musicaElectro = document.getElementById("boton2");
 
-
 musicaRock.addEventListener('click', ()=>{
-	window.location.href = "https://open.spotify.com/playlist/6oF4i9FMoWGfvwUUI7IpgH?si=3042e76d75f54b11";
+
+		window.location.href = "https://open.spotify.com/playlist/6oF4i9FMoWGfvwUUI7IpgH?si=3042e76d75f54b11";
 });
 
 musicaElectro.addEventListener('click', ()=>{
--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-let usuario = prompt ("Ingrese su nombre:");
+let usuario = localStorage.getItem ('usuario') || '';
 
-usuario = JSON.parse (localStorage.getItem('usuario')) || []
+const msjMensaje = document.querySelector('#msjBienvenida');
+
+if (usuario==''){
+    usuario = prompt ("Ingrese su nombre:")
+    localStorage.setItem('usuario',usuario);
+    actualizarMensaje();
+}
+actualizarMensaje();
+console.log(usuario);
+
+
+function actualizarMensaje () {
+    msjMensaje.textContent=`Hola ${usuario}`;
+}
+
+const boton = document.querySelector("#card1")
+
+boton.addEventListener("click", () => {
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+    )
+}
+)
