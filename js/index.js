2a7947f339b7c1ccc11302d209abaca1322ba3d7
Sofia Furno <furnosofia@gmail.com>
2022-10-12 14:28:00 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,34 +1,4 @@
-let usuario = localStorage.getItem ('usuario') || '';
-
-const msjMensaje = document.querySelector('#msjBienvenida');
-
-
-if (usuario==''){
-    Swal.fire({
-    title: 'Ingrese su nombre:',
-    input: 'name',
-    inputLabel: 'Su nombre',
-    inputPlaceholder: 'Ingrese su nombre',
-    preConfirm: (name) => {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            if (name === 'usuario') {
-              swal.showValidationError(
-                'This name is already taken.'
-              )
-            }
-            resolve()
-        }, 2000)
-      })
-    }
-  })
-    localStorage.setItem('usuario',usuario);
-    actualizarMensaje();
-}
-actualizarMensaje();
-console.log(usuario);
-
-
-function actualizarMensaje () {
-    msjMensaje.textContent=`Hola ${usuario}`;
-}
+Swal.fire(
+  '¡Bienvenido/a!',
+  'success'
+)
